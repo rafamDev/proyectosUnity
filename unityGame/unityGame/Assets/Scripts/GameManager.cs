e7a1a607a28ba6c4cfa2d0be37e963cf6c0e909f
RafaelMayo <rafamayo7@gmail.com>
2021-03-05 22:11:50 +0100

    already finished

--- unityGame/unityGame/Assets/Scripts/GameManager.cs
+++ unityGame/unityGame/Assets/Scripts/GameManager.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour{ 
+
     public Renderer fondo;
     public GameObject objetoColumna;
     public GameObject tronco;
@@ -13,22 +13,19 @@ public class GameManager : MonoBehaviour
     public GameObject menuGameOver;
 
     protected void Start(){
-       for(int i=0; i < 19; i++){
-           Instantiate(objetoColumna, new Vector2(0 + i, 0), Quaternion.identity);
-        }
-        for(int i=2; i < 50; i++){
+       for(int i=0; i < 2; i++){
+           Instantiate(objetoColumna, new Vector2(i + 1, 0), Quaternion.identity);
+       }
+       for(int i=1; i <= 40; i++){
            troncos.Add(Instantiate(tronco, new Vector2(i * 20,-2.00f), Quaternion.identity)); 
-         }
+       }
     }
 
     protected void Update(){
       if(gameOver == false){ 
-           fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.15f,0) * Time.deltaTime;
+          fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.15f,0) * Time.deltaTime;
              for(int i=0; i < troncos.Count; i++){
-                troncos[i].transform.position += new Vector3(-1,0,0) * Time.deltaTime * 5;
-             }
-             if(troncos.Count == 47){
-                gameOver = true;
+                 troncos[i].transform.position += new Vector3(-1,0,0) * Time.deltaTime * 6;
              }
       }
       if(gameOver == true){
@@ -41,8 +38,5 @@ public class GameManager : MonoBehaviour
       }  
      
    }
-  
-   
-
 
 }
--- unityGame/unityGame/Assets/Scripts/Jugador.cs
+++ unityGame/unityGame/Assets/Scripts/Jugador.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jugador : MonoBehaviour
-{
+public class Jugador : MonoBehaviour{
     private Animator jugador;
     private Rigidbody2D cuerpoJugador;
     public float fuerzaSalto;
     public bool estaSuelo;
-    public int numero;
-    
+   
     protected void Start(){
         jugador = GetComponent<Animator>();
         cuerpoJugador = GetComponent<Rigidbody2D>();
@@ -20,20 +18,24 @@ public class Jugador : MonoBehaviour
             jugador.SetBool("estaSaltando",true);
             cuerpoJugador.AddForce(new Vector2(0,fuerzaSalto));
             estaSuelo = false;
+            fuerzaSalto = 0;
         }
     }
-
+   
     private void OnCollisionStay2D(Collision2D other) {
         estaSuelo = true;
     }
     
     private void OnCollisionEnter2D(Collision2D collision) {
         if(collision.gameObject.tag == "Suelo"){
+            fuerzaSalto = 500;
             jugador.SetBool("estaSaltando",false);
             estaSuelo = true;
         }
         if(collision.gameObject.tag == "Obstaculo"){
-           GameManager.gameOver = true;
+            GameManager.gameOver = true;
+            jugador.speed = 0;
         }
     }
+
 }
